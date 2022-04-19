6f28005a6a159827388becebc66b19a7be08e26a
Sebastian Schlachter <sebastian.schlachter@stud.unibas.ch>
2022-04-19 09:41:32 +0200

    Tested Killer Sudoku by Seungjae Kwak

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     static String br = "\n";
 
     public static void main(String[] args) {
-        String probNum = "02";
+        String probNum = "03";
         ArrayList<String> clauses = new ArrayList<>();
 
         clauses.addAll(oneNumberPerEntry());
